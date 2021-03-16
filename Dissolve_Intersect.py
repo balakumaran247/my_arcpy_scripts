30944e724539dd374107590f41dc9a94f866192f
balakumaran <balakumaran247@gmail.com>
2021-03-16 23:39:59 +0530

    Added: Pivot Table functionality

--- Dissolve_Intersect.py
+++ Dissolve_Intersect.py
@@ -2,7 +2,9 @@ import os
 import arcpy
 from arcpy import env
 import pandas as pd
-import os, shutil
+import os
+import shutil
+import numpy as np
 
 no_of_terms = 5
 year = "2009_10"
@@ -35,17 +37,18 @@ for file_path in rm_files:
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 original_files = [t0_file, t1_file, t2_file, t3_file, t4_file, t5_file]
-for count in range(len(original_files)):
+for count in range(no_of_terms+1):
     original_file = original_files[count]
     term_name = "T"+str(count)
-    o_path = os.path.join(drive_path,year,folder_name,term_name,original_file+".shp")
-    arcpy.CopyFeatures_management(o_path,original_file+".shp")
+    o_path = os.path.join(drive_path, year, folder_name,
+                            term_name, original_file+".shp")
+    arcpy.CopyFeatures_management(o_path, original_file+".shp")
 
 code_variants = {
-    'Code_1' : '!Code_1!',
-    'Code_12' : '!Code_12!',
-    'Code_13' : '!Code_13!',
-    'Code_1_2' : '!Code_1_2!',
+    'Code_1': '!Code_1!',
+    'Code_12': '!Code_12!',
+    'Code_13': '!Code_13!',
+    'Code_1_2': '!Code_1_2!',
 }
 
 inputList = arcpy.ListFeatureClasses()
@@ -54,7 +57,8 @@ for file in inputList:
     for i in arcpy.ListFields(file):
         if i.name in list(code_variants.keys()):
             arcpy.AddField_management(file, "Code", "TEXT")
-            arcpy.CalculateField_management(file, "Code",code_variants[i.name],"PYTHON")
+            arcpy.CalculateField_management(
+                file, "Code", code_variants[i.name], "PYTHON")
             arcpy.DeleteField_management(file, list(code_variants.keys()))
     arcpy.AddField_management(file, "Descrpn", "TEXT")
     expr = "getClass(!Code!)"
@@ -71,39 +75,52 @@ for file in inputList:
         elif desc in forest_des:
             return 'Forest'
         elif desc in water_des:
-            return 'Water'
+            return 'WB_Water'
         elif desc in waste_des:
-            return 'Waste'
+            return 'WL_Waste'
         else:
             return 'Unknown'"""
     arcpy.CalculateField_management(file, "Descrpn", expr, "PYTHON", codeblock)
 
 outputList = ["T0.shp", "T1.shp", "T2.shp", "T3.shp", "T4.shp", "T5.shp"]
-TToutputList = ["T0_T1.shp", "T1_T2.shp", "T2_T3.Shp", "T3_T4.shp", "T4_T5.shp"]
+TToutputList = ["T0_T1.shp", "T1_T2.shp",
+                "T2_T3.Shp", "T3_T4.shp", "T4_T5.shp"]
 dissolveInList = ["T0.dbf", "T1.dbf", "T2.dbf", "T3.dbf", "T4.dbf", "T5.dbf"]
 dissolveOutList = ["T0.xls", "T1.xls", "T2.xls", "T3.xls", "T4.xls", "T5.xls"]
-IntersectInList = ["T0_T1.dbf", "T1_T2.dbf", "T2_T3.dbf", "T3_T4.dbf", "T4_T5.dbf"]
-IntersectOutList = ["T0_T1.xls", "T1_T2.xls", "T2_T3.xls", "T3_T4.xls", "T4_T5.xls"]
+IntersectInList = ["T0_T1.dbf", "T1_T2.dbf",
+                    "T2_T3.dbf", "T3_T4.dbf", "T4_T5.dbf"]
+IntersectOutList = ["T0_T1.xls", "T1_T2.xls",
+                    "T2_T3.xls", "T3_T4.xls", "T4_T5.xls"]
+pivotOutList = ["T0_T1.csv", "T1_T2.csv",
+                "T2_T3.csv", "T3_T4.csv", "T4_T5.csv"]
+
 
 def dissolve_func(input, output):
     arcpy.Dissolve_management(input, output, ["Descrpn"])
     arcpy.AddField_management(output, "Arrr", "DOUBLE")
-    arcpy.CalculateField_management(output,'Arrr','!shape.area@hectares!','PYTHON')
+    arcpy.CalculateField_management(
+        output, 'Arrr', '!shape.area@hectares!', 'PYTHON')
+
 
 def intersect_func(input, output):
     arcpy.Intersect_analysis(input, output)
     arcpy.AddField_management(output, "Arrr", "DOUBLE")
-    arcpy.CalculateField_management(output,'Arrr','!shape.area@hectares!','PYTHON')
+    arcpy.CalculateField_management(
+        output, 'Arrr', '!shape.area@hectares!', 'PYTHON')
+
 
 for rep in range(no_of_terms+1):
     dissolve_func(inputList[rep], outputList[rep])
     arcpy.TableToExcel_conversion(dissolveInList[rep], dissolveOutList[rep])
     if rep != 0:
         intersect_func([inputList[rep-1], inputList[rep]], TToutputList[rep-1])
-        arcpy.TableToExcel_conversion(IntersectInList[rep-1], IntersectOutList[rep-1])
+        arcpy.TableToExcel_conversion(
+            IntersectInList[rep-1], IntersectOutList[rep-1])
 
 combined_df = pd.DataFrame()
-combined_dissolve_out = os.path.join(out_path, str("A_Combined_Dissolve") + ".xls")
+combined_dissolve_out = os.path.join(
+    out_path, folder_name + "_Combined" + ".xls")
+
 
 def excel_func(inputDF):
     inputDF = inputDF.drop(['FID'], axis=1)
@@ -114,10 +131,26 @@ def excel_func(inputDF):
     inputDF.columns = new_header
     return inputDF
 
-excel_list = [os.path.join(env_path,i) for i in dissolveOutList]
+
+excel_list = [os.path.join(env_path, i) for i in dissolveOutList]
 for ind, i in enumerate(excel_list):
     if ind < no_of_terms+1:
-        combined_df = combined_df.append(excel_func(pd.read_excel(i)), ignore_index=True)
+        combined_df = combined_df.append(
+            excel_func(pd.read_excel(i)), ignore_index=True)
 
 combined_df = combined_df.fillna(0)
+for rep in range(no_of_terms):
+    combined_df.loc[outputList[rep+1]]=combined_df.loc[rep+1]-combined_df.loc[rep]
 combined_df.to_excel(combined_dissolve_out)
+
+for term in range(no_of_terms):
+    pivot_file = os.path.join(env_path, IntersectOutList[term])
+    df = pd.read_excel(pivot_file)
+    pivot_df = df.pivot_table(index=['Descrpn'], columns=['Descrpn_1'], values=
+                            ['Arrr'], aggfunc='sum', fill_value=0)
+    pivot_df.loc[:, 'Total'] = pivot_df.sum(axis=1)
+    pivot_df.loc['Total', :] = pivot_df.sum(axis=0)
+    pivot_df[pivot_df.columns] = pivot_df[pivot_df.columns].replace(
+                                                                ['0', 0], np.nan)
+    pivot_df.to_csv(os.path.join(
+        out_path, folder_name + '_' + pivotOutList[term]))
